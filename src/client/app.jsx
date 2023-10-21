915308f574146c73898e04ce43a7a9e9fbb2e100
Joseph Paul <joepubacc@gmail.com>
2023-10-21 12:46:23 -0400

    build sidebar

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -2,12 +2,14 @@
 
 import React from 'react';
 // import Login from './components/login';
+import Sidebar from './components/sidebar';
 
 function App() {
   return (
     <>
       <h1>Pass the Plate</h1>
       {/* <Login /> */}
+      <Sidebar />
     </>
   );
 }
