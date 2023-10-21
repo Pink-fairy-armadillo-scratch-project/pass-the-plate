b3890c61728a66719930f18f605b144dfeadd6af
Joseph Paul <joepubacc@gmail.com>
2023-10-21 13:20:38 -0400

    split home and login into two pages

---
+++ src/client/loginProvider.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Login from './components/login';
+
+function loginProvider() {
+  return (
+    <Login />
+  );
+}
+
+export default loginProvider;
---
+++ src/loginRoot.jsx
@@ -0,0 +1,18 @@
+// entry point for the front end
+
+// import the stuff we need
+import React from 'react';
+import { createRoot } from 'react-dom';
+import LoginProvider from './client/loginProvider';
+
+
+// import styles
+import './client/styles/homeStyles.scss';
+
+
+const cheese = document.createElement('main');
+document.querySelector('body').append(cheese);
+
+const root = createRoot(cheese);
+
+root.render(<LoginProvider />);
