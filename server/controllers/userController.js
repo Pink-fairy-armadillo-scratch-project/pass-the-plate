29e12bace6f8aa1ccc6ef0f31f7b843a9f6fdbf1
Andrew Lo <andrewlo@Carolines-iMac.lan>
2023-10-21 13:15:31 -0700

    made some edits

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,6 +4,7 @@
 const userController = {
   verifyUser(req, res, next) {
     console.log("verified")
+    console.log("request body in controller: ", req.body)
     res.locals.activitySave = "posted successfully!"
     return next()
   }
--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const userController = require('./controllers/userController')
 
+app.use(bodyParser.urlencoded());
 app.use(express.json());
 
 app.post('/login', userController.verifyUser, (req, res) => {
 
-    console.log("reached server")
+    console.log("request body in server: ", req.body)
 
     res.status(200).json(res.locals.activitySave)
 })
