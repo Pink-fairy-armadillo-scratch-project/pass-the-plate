20a8a9089c216cb673af1476ba4d6d087d6274f5
Andrew Lo <andrewlo@Carolines-iMac.lan>
2023-10-21 14:32:54 -0700

    request is coming from fake database

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,13 +1,34 @@
 
 
-
-const userController = {
-  verifyUser(req, res, next) {
-    console.log("verified")
-    console.log("request body in controller: ", req.body)
-    res.locals.activitySave = "posted successfully!"
-    return next()
-  }
+const fakeListing = {
+  
 }
+
+
+
+const userController = {};
+
+  let userReqBod;
+
+  userController.verifyUser = (req, res, next) => {
+    console.log("we shouldnt be hitting this")
+    // console.log("request body in Originalcontroller: ", req.body)
+    userReqBod = req.body;
+    res.redirect('/redirect')
+    return next();
+  };
+
+  userController.findListings = (req, res, next) => {
+    console.log("request body in RedirectController: ", userReqBod)
+
+    let username = userReqBod[username]
+
+
+
+    //Query SQL DB for SELECT * WHERE userReqBody === zipcode_id
+    res.locals.listings = "database response"
+    return next()
+  };
+
 
 module.exports = userController;
--- server/server.js
+++ server/server.js
@@ -5,15 +5,30 @@ const userController = require('./controllers/userController');
 
 app.use(bodyParser.urlencoded());
 app.use(express.json());
-app.use(bodyParser.urlencoded());
 
 app.post('/login', userController.verifyUser, (req, res) => {
 
     console.log("request body in server: ", req.body)
 
-    res.status(200).json(res.locals.activitySave)
+    // res.status(200).json(res.locals.activitySave)
 })
 
+
+
+
+app.get('/redirect', userController.findListings, (req, res) => {
+    console.log('made it to redirect')
+    console.log("this is the find listing controller function:", req.body)
+
+    res.status(200).json(res.locals.listings)
+})
+
+// app.get('/redirect', (req, res) => {
+//     console.log('made it to redirect')
+//     console.log("request body", req.body)
+
+//     res.status(200)
+// })
 
 app.listen(1234, () => {
   console.log('Listening on port 1234');
